9f659e2fa38319214ab9e33277fe579b5d6cb0a5
Gauransh Arora <gauransharora01@gmail.com>
2026-01-31 17:25:24 +0530

    1

---
+++ Strings/Len.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Len 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String s = sc.next(); 
+        System.out.println(s.length());
+    }
+    
+}
